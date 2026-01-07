832780b55d1c2660f634d597f6de553830600bb1
kabincodes <kabinworkspace7@gmail.com>
2026-01-07 15:43:59 +0100

    Add second batch of books.

--- src/data/books.ts
+++ src/data/books.ts
@@ -12,6 +12,22 @@ import outliers from "@/assets/books/outliers.jpeg";
 import sevenHabits from "@/assets/books/seven-habits-teens.jpg";
 import shoeDog from "@/assets/books/shoe-dog.jpeg";
 import steveJobs from "@/assets/books/steve-jobs-biography.jpg";
+import alchemist from "@/assets/books/alchemist.jpg";
+import asAManThinketh from "@/assets/books/as-a-man-thinketh.jpg";
+import beyondOrder from "@/assets/books/beyond-order.webp";
+import brothersKaramazov from "@/assets/books/brothers-karmazov.webp";
+import cleanCode from "@/assets/books/clean-code.webp";
+import elevenMinutes from "@/assets/books/eleven-minutes.webp";
+import howToStopWorrying from "@/assets/books/how-to-stop-worrying.webp";
+import howToWinFriends from "@/assets/books/how-to-win-friends.jpg";
+import killingCommendatore from "@/assets/books/killing-commendatore.webp";
+import losingVirginity from "@/assets/books/losing-virginity.webp";
+import norwegianWood from "@/assets/books/norwegian-wood.webp";
+import snowball from "@/assets/books/snowball.jpg";
+import superiorMan from "@/assets/books/superior-man.webp";
+import tuesdayWithMorrie from "@/assets/books/tuesday-with-morrie.webp";
+import twelveRules from "@/assets/books/twelve-rules.webp";
+import zahir from "@/assets/books/zahir.webp";
 
 export const books = [
   {
@@ -125,5 +141,133 @@ export const books = [
     description: "",
     buttonText: "JS Insights",
     url: javascriptNinjas,
+  },
+  {
+    id: "alchemist",
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    description: "",
+    buttonText: "Reflections",
+    url: alchemist,
+  },
+  {
+    id: "as-a-man-thinketh",
+    title: "As a Man Thinketh",
+    author: "James Allen",
+    description: "",
+    buttonText: "Core Ideas",
+    url: asAManThinketh,
+  },
+  {
+    id: "beyond-order",
+    title: "Beyond Order",
+    author: "Jordan B. Peterson",
+    description: "",
+    buttonText: "Life Principles",
+    url: beyondOrder,
+  },
+  {
+    id: "brothers-karamazov",
+    title: "The Brothers Karamazov",
+    author: "Fyodor Dostoevsky",
+    description: "",
+    buttonText: "Philosophical Themes",
+    url: brothersKaramazov,
+  },
+  {
+    id: "clean-code",
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    description: "",
+    buttonText: "Engineering Discipline",
+    url: cleanCode,
+  },
+  {
+    id: "eleven-minutes",
+    title: "Eleven Minutes",
+    author: "Paulo Coelho",
+    description: "",
+    buttonText: "Reflections",
+    url: elevenMinutes,
+  },
+  {
+    id: "how-to-stop-worrying",
+    title: "How to Stop Worrying and Start Living",
+    author: "Dale Carnegie",
+    description: "",
+    buttonText: "Practical Wisdom",
+    url: howToStopWorrying,
+  },
+  {
+    id: "how-to-win-friends",
+    title: "How to Win Friends & Influence People",
+    author: "Dale Carnegie",
+    description: "",
+    buttonText: "People Skills",
+    url: howToWinFriends,
+  },
+  {
+    id: "killing-commendatore",
+    title: "Killing Commendatore",
+    author: "Haruki Murakami",
+    description: "",
+    buttonText: "Themes & Motifs",
+    url: killingCommendatore,
+  },
+  {
+    id: "losing-my-virginity",
+    title: "Losing My Virginity",
+    author: "Richard Branson",
+    description: "",
+    buttonText: "Founder Lessons",
+    url: losingVirginity,
+  },
+  {
+    id: "norwegian-wood",
+    title: "Norwegian Wood",
+    author: "Haruki Murakami",
+    description: "",
+    buttonText: "Reflections",
+    url: norwegianWood,
+  },
+  {
+    id: "snowball",
+    title: "The Snowball",
+    author: "Alice Schroeder",
+    description: "",
+    buttonText: "Investment Lessons",
+    url: snowball,
+  },
+  {
+    id: "superior-man",
+    title: "The Way of the Superior Man",
+    author: "David Deida",
+    description: "",
+    buttonText: "Masculine Growth",
+    url: superiorMan,
+  },
+  {
+    id: "tuesday-with-morrie",
+    title: "Tuesdays with Morrie",
+    author: "Mitch Albom",
+    description: "",
+    buttonText: "Life Lessons",
+    url: tuesdayWithMorrie,
+  },
+  {
+    id: "twelve-rules",
+    title: "12 Rules for Life",
+    author: "Jordan B. Peterson",
+    description: "",
+    buttonText: "Life Framework",
+    url: twelveRules,
+  },
+  {
+    id: "zahir",
+    title: "The Zahir",
+    author: "Paulo Coelho",
+    description: "",
+    buttonText: "Reflections",
+    url: zahir,
   },
 ];
